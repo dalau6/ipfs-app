d9c29852488919d0c5851090a78425b9ea5fcfaf
David <dalau6@gmail.com>
2019-11-30 21:50:24 -0800

    ipfs test connection.

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const ipfsClient = require('ipfs-http-client');
 
 const ipfs = ipfsClient('http://localhost:5001');
+
+const addFile = async () => {
+    const file = { path: 'testfile', content: Buffer.from('hello') };
+    const filesAdded = await ipfs.add(file);
+    console.log(filesAdded[0]);
+}
